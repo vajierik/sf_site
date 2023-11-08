f0153ce2ef11d7db5d78381ca8db90ef228b5f8e
dilitrium <dilitrium@gmail.com>
2023-11-08 18:19:23 +0300

    Change clean_tmp_websso.bat from litium to vajierik

--- clean_tmp_websso.bat
+++ clean_tmp_websso.bat
@@ -1,3 +1,4 @@
 forfiles /p c:\ftas01\prod\secur_eb_new\webservice-sso-proxy\temp\ /m *.tmp /s /d -2 /c "cmd /c del @path /q"
 forfiles /p c:\ftas01\prod\secur_eb_new\webservice-sso-proxy\logs\ /m *.log /d -2 /c "cmd /c del @path /q"
 forfiles /p c:\ftas01\prod\secur_eb_new\webservice-sso-proxy\logs\ /m *.txt /d -2 /c "cmd /c del @path /q"
+forfiles /p c:\ftas01\prod\secur_eb_new\webservice-sso-proxy\logs\ /m *.trc /d -2 /c "cmd /c del @path /q"
