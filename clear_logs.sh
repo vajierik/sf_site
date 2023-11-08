051ebbfcfe0430e0b2bf48f17a098dd05a1aa9ad
dilitrium <dilitrium@gmail.com>
2023-11-08 18:20:58 +0300

    Change clear_logs.sh from litium to vajierik

--- clear_logs.sh
+++ clear_logs.sh
@@ -12,6 +12,7 @@ find . -name 'osb_server1.log0*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 find . -name 'osb_server1.log1*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 find . -name 'osb_server1.out0*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 find . -name 'access.log0*' -mtime +3 -exec rm -f {} \; 2>/dev/null
+find . -name 'access.trc*' -mtime +3 -exec rm -f {} \; 2>/dev/null
 cat osb_server1.out >> osb_server1.out0_`date +%Y.%m.%d-%H.%M` && cat /dev/null > osb_server1.out
 #cat /dev/null > osb_server1.out
 
